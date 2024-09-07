479dbc0e87d140555351a1c51f9623be0f1cd9ac
Your Name <shado@LAPTOP-U7GG9IKM>
2024-09-07 21:03:53 +0800

    initial commit

--- app.js
+++ app.js
@@ -13,3 +13,15 @@ console.log(age);
 
 let isStudent = true;
 console.log(isStudent);
+
+let colors = ["red", "blue", "white"];
+console.log(colors);
+
+const num1 = 3;
+const num2 = 4;
+console.log(num1+num2);
+
+const favnum = 5;
+const favnum2 = 6;
+const quotient = favnum/favnum2;
+console.log(quotient);
