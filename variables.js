1c3c6bdb39247d570031804ab61fecb3aa045442
Your Name <shado@LAPTOP-U7GG9IKM>
2024-09-13 13:40:36 +0800

    initial commit act 2

---
+++ variables.js
@@ -0,0 +1,9 @@
+let name = "Karl Lowie Inay";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "male";
+let twitterHandle = "@l3_yart0p";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
