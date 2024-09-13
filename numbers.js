528adb59a8ae8f9d33bcecc41bd75699085d67d4
Your Name <shado@LAPTOP-U7GG9IKM>
2024-09-13 13:16:51 +0800

    initial commit act 3

---
+++ numbers.js
@@ -0,0 +1,20 @@
+let firstFavNum = 10;
+let secondFavNum = 5;
+
+//Addition
+console.log(firstFavNum+secondFavNum);
+
+//Subtraction
+console.log(firstFavNum-secondFavNum);
+
+//Multiplication
+console.log(firstFavNum*secondFavNum);
+
+//Division
+console.log(firstFavNum/secondFavNum);
+
+//Modulus
+console.log(firstFavNum%secondFavNum);
+
+//Exponent
+console.log(firstFavNum**secondFavNum);
